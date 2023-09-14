95c1a29f03b9ba04b799c5cce114fe5bf3985cc8
LexAeterna731 <Delirium731@yandex.ru>
2023-09-14 17:40:27 +0700

    fix views for tests

--- lang/ru/validation.php
+++ lang/ru/validation.php
@@ -143,4 +143,21 @@ return [
     'uploaded' => 'Не удалось загрузить :attribute.',
     'url' => ':attribute должен быть допустимым URL.',
     'uuid' => ':attribute должен быть действительным UUID.',
+
+     /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [
+        'password' => 'Пароль',
+        'Password' => 'Пароль',
+
+    ],
 ];
